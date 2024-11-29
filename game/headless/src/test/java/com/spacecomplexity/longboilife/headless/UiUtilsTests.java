755a17cf454c13f86876010e7989eac3d7e821c7
nk-t14 <aclatm.20@gmail.com>
2024-11-29 12:26:39 +0000

    add uiutits class

---
+++ game/headless/src/test/java/com/spacecomplexity/longboilife/headless/UiUtilsTests.java
@@ -0,0 +1,11 @@
+package com.spacecomplexity.longboilife.headless;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+public class UiUtilsTests extends AbstractHeadlessGdxTest {
+
+}
