9466afde01cb4a32d3dfc2af135ae575079ddb34
TameU <tamerlanurazbayev@gmail.com>
2024-11-29 14:07:45 +0000

    Cleaned up UIEventsMenu

--- game/core/src/main/java/com/spacecomplexity/longboilife/game/ui/game/UIEventsMenu.java
+++ game/core/src/main/java/com/spacecomplexity/longboilife/game/ui/game/UIEventsMenu.java
@@ -1,19 +1,12 @@
 package com.spacecomplexity.longboilife.game.ui.game;
 
-import org.w3c.dom.events.UIEvent;
-
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.Viewport;
-import com.spacecomplexity.longboilife.game.globals.GameState;
 import com.spacecomplexity.longboilife.game.ui.UIElement;
-import com.spacecomplexity.longboilife.game.globals.Filepaths;
 
 public class UIEventsMenu extends UIElement{
 
@@ -31,7 +24,6 @@ public class UIEventsMenu extends UIElement{
 
         // Style and place the table
         table.setBackground(skin.getDrawable("panel1"));
-        // Feature - update height based on number of entries to display
         table.setSize(220, 100);
         placeTable();
     }
