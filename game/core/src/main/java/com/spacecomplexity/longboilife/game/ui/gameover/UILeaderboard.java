e5258bf6ec0686f39251a2e43011f97a59a67686
TameU <tamerlanurazbayev@gmail.com>
2024-12-06 13:32:55 +0000

    Display scores by descending order and handle when user is already in leaderboard

--- game/core/src/main/java/com/spacecomplexity/longboilife/game/ui/gameover/UILeaderboard.java
+++ game/core/src/main/java/com/spacecomplexity/longboilife/game/ui/gameover/UILeaderboard.java
@@ -7,14 +7,16 @@ import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.spacecomplexity.longboilife.game.ui.UIElement;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Scanner;
+import java.util.TreeSet;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
 import com.spacecomplexity.longboilife.game.globals.Filepaths;
 import com.spacecomplexity.longboilife.game.globals.GameState;
 
@@ -32,17 +34,25 @@ public class UILeaderboard extends UIElement {
     public UILeaderboard(Viewport uiViewport, Table parentTable, Skin skin) {
         super(uiViewport, parentTable, skin);
 
-        Integer index = 1;
+        String username = System.getProperty("user.name");
+        String currentUser = "";
+        String prevScore = "";
         String line = "";
         String allScores = "----------Leaderboard----------\n";
+        TreeSet<String> sortedNames = new TreeSet<String>();
+        TreeSet<String> sortedScores = new TreeSet<String>();
 
         // Read in names and scores from the leaderboard
         try {
           BufferedReader leaderboardReader = new BufferedReader(new FileReader(Filepaths.LEADERBOARD_DATA));
           while ((line = leaderboardReader.readLine()) != null) {  
             String[] entry = line.split(",");
-            allScores += index + "." + entry[0] + "    " + entry[1] + "\n";
-            index += 1;
+            // Save old score if user has already played the game
+            if (entry[0] == username) {
+              prevScore = entry[1];
+            }
+            sortedNames.add(entry[0]);
+            sortedScores.add(entry[1]);
           }
           leaderboardReader.close();
         } catch (IOException e) {
@@ -53,15 +63,27 @@ public class UILeaderboard extends UIElement {
         // Write new score to leaderboard
         try {
           BufferedWriter leaderboardWriter = new BufferedWriter(new FileWriter(Filepaths.LEADERBOARD_DATA, true));
-          String username = System.getProperty("user.name");
           String score = String.format("%.2f", GameState.getState().satisfactionScore * 100);
-          leaderboardWriter.append("\n" + username + "," + score);
-          leaderboardWriter.close();
-          allScores += index + "." + username + "    " + score;
+          if (username == currentUser && score.compareTo(prevScore) == 1) {
+            leaderboardWriter.append("\n" + username + "," + score);
+            leaderboardWriter.close();
+            sortedNames.add(username);
+            sortedScores.add(score);
+          }
         } catch (IOException e) {
           // TODO: Check for file as part of testing
           allScores += "MISSING FILE - WRITE";
         };
+
+        // Save sorted names and scores
+        List<String> nameList = new ArrayList<String>(sortedNames);
+        List<String> scoreList = new ArrayList<String>(sortedScores);
+        // Reverse lists to display in descending order
+        Collections.reverse(nameList);
+        Collections.reverse(scoreList);
+        for (int i = 0; i < nameList.size(); i ++) {
+          allScores += (i + 1) + "." + nameList.get(i) + "    " + scoreList.get(i) + "\n";
+        }
         
         // Initialise leaderboard
         Label label = new Label(String.format(allScores), skin);
@@ -75,8 +97,8 @@ public class UILeaderboard extends UIElement {
         // Style and place the table
         table.setBackground(skin.getDrawable("panel1"));
         // Update height based on number of entries to display - minimum 4
-        if (index > 3) {
-            table.setSize(220, 100 + (index-3)*15);
+        if (nameList.size() > 3) {
+            table.setSize(220, 100 + (nameList.size()-3)*15);
         }
         else {
             table.setSize(220, 100);
