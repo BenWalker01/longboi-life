d70c73d85f9ccdc0c049b111a870025fb4c65d11
tim4ik <tim4ik@yahoo.com>
2025-01-06 23:59:30 +0200

    feat: money change pop up

---
+++ game/core/src/main/java/com/spacecomplexity/longboilife/game/ui/game/UIMoneyPopUp.java
@@ -0,0 +1,77 @@
+package com.spacecomplexity.longboilife.game.ui.game;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.spacecomplexity.longboilife.game.globals.GameState;
+import com.spacecomplexity.longboilife.game.globals.MainTimer;
+import com.spacecomplexity.longboilife.game.ui.UIElement;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class UIMoneyPopUp extends UIElement {
+    private Label label;
+
+    private long showTime = 5*60*1000;
+
+    /**
+     * Initialise money menu elements.
+     *
+     * @param uiViewport  the viewport used to render UI.
+     * @param parentTable the table to render this element onto.
+     * @param skin        the provided skin.
+     */
+    public UIMoneyPopUp(Viewport uiViewport, Table parentTable, Skin skin) {
+        super(uiViewport, parentTable, skin);
+
+        // Initialise money label
+        label = new Label(null, skin);
+        label.setFontScale(1.2f);
+        label.setColor(Color.WHITE);
+
+        // Place label onto table
+        table.add(label).align(Align.center);
+
+        table.setVisible(false);
+
+        // Style and place the table
+        table.setBackground(skin.getDrawable("panel1"));
+        table.setSize(120, 40);
+        placeTable();
+    }
+
+    public void render() {
+        long currentTime = MainTimer.getTimerManager().getTimer().getTimeLeft();
+        if (showTime - currentTime > 5000) {
+            hide();
+        }
+    }
+
+    @Override
+    protected void placeTable() {
+        table.setPosition(uiViewport.getWorldWidth() - table.getWidth(), uiViewport.getWorldHeight() - table.getHeight() - 100);
+    }
+
+    public void show(int moneyChange) {
+        if (moneyChange == 0) {
+            return;
+        }
+        table.setVisible(true);
+        showTime = MainTimer.getTimerManager().getTimer().getTimeLeft();
+        if (moneyChange > 0) {
+            label.setColor(Color.GREEN);
+            label.setText("+" + NumberFormat.getCurrencyInstance(Locale.UK).format(moneyChange));
+        } else {
+            label.setColor(Color.RED);
+            label.setText(NumberFormat.getCurrencyInstance(Locale.UK).format(moneyChange));
+        }
+    }
+
+    public void hide() {
+        table.setVisible(false);
+    }
+}
